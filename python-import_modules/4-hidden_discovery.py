a140b664f15fd4bcbf50ad02b73c11fc88a1c225
NACIMSAAFI <7715@holbertonstudents.com>
2024-01-26 23:14:11 +0100

    4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
+    import hidden_4
     for i in dir(hidden_4):
-        if i[:2] != '__':
+        if i[:2] != "__":
             print(i)
+            
