73ac7650d9e4260eee1d69bf053374156fa1a7ff
NACIMSAAFI <7715@holbertonstudents.com>
2024-01-26 20:08:27 +0100

    Add 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    from sys import argv
 
-    if len(sys.argv) - 1 == 0:
+    if len(argv) - 1 == 0:
         print("0 arguments.")
-    elif len(sys.argv) - 1 == 1:
+    elif len(argv) - 1 == 1:
         print("1 argument:")
     else:
-        print("{} arguments".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+        print("{} arguments".format(len(argv) - 1))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
