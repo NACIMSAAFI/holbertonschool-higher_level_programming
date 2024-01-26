c3d111240b56eb7d949b80f79e2649cb162c106d
NACIMSAAFI <7715@holbertonstudents.com>
2024-01-26 20:13:40 +0100

    Add 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,9 +2,9 @@
 if __name__ == "__main__":
     from sys import argv
     if len(argv) - 1 == 0:
-        print("0 arguments.")
+        print("{} arguments.".format(len(argv) - 1))
     elif len(argv) - 1 == 1:
-        print("1 argument:")
+        print("{} argument:".format(len(argv) - 1))
     else:
         print("{} arguments".format(len(argv) - 1))
     for i in range(1, len(argv)):
