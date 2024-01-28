a3e44988d63ed279a9fda5d25c46a879ac6a03ef
NACIMSAAFI <7715@holbertonstudents.com>
2024-01-28 18:40:32 +0100

    Add 7-add_tuple.py

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    result = (0, 0)
+
+    if len(tuple_a) > 0:
+        result = (result[0] + tuple_a[0], result[1])
+    if len(tuple_b) > 0:
+        result = (result[0] + tuple_b[0], result[1])
+
+    if len(tuple_a) > 1:
+        result = (result[0], result[1] + tuple_a[1])
+    if len(tuple_b) > 1:
+        result = (result[0], result[1] + tuple_b[1])
+
+    return result
