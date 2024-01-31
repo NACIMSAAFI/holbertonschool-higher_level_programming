25586dd13da4dc37b6dba8936f2b5b85212fc7df
NACIMSAAFI <7715@holbertonstudents.com>
2024-01-31 23:14:28 +0100

    Add 0-safe_print_list.py

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    cnt = 0
+    nb_print = 0
+    while cnt < x:
+        try:
+            print("{}".format(my_list[cnt]), end="")
+            nb_print += 1
+        except:
+            break
+        cnt += 1
+    print()
+    return nb_print
