97d766351d22c61a3ab0f982f5c356a786866711
NACIMSAAFI <7715@holbertonstudents.com>
2024-01-31 23:43:21 +0100

    Add 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    number = 0
-    for n in range(0, x):
+    cnt = 0
+    nb_print = 0
+    while cnt < x:
         try:
-            print("{}".format(my_list[n]), end="")
-            number += 1
+            print("{}".format(my_list[cnt]), end="")
+            nb_print += 1
         except IndexError:
             break
+        cnt += 1
     print()
-    return number
+    return nb_print
