c386ec4ac95c7799f3de9c7a6f2dc70a39bc720f
NACIMSAAFI <7715@holbertonstudents.com>
2024-02-04 18:51:43 +0100

    Add 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""
+    Adds two integers or floats.
+
+    Parameters:
+    - a (int or float): The first number.
+    - b (int or float, optional): The second number. Defaults to 98.
+
+    Returns:
+    - int: The sum of a and b as an integer.
+    """
+
 
 def add_integer(a, b=98):
     """
