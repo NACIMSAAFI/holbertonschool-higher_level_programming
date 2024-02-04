bbaca75420a006c464987c5cf6b58bb2a98443fc
NACIMSAAFI <7715@holbertonstudents.com>
2024-02-04 12:23:23 +0100

    3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This is the Square class with a private size attribute.
+
+Parameters:
+    - size (int, optional): The size of the square (default is 0).
+"""
+
+
+class Square:
+    """
+    This is the Square class with a private size attribute.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes the Square instance.
+
+        Parameters:
+        - size (int, optional): The size of the square (default is 0).
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+        - int: The area of the square.
+        """
+        return self.__size**2
