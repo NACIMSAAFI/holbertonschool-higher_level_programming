caf79aba24cd8b99aa2df479def1d3a0d3fa7de6
NACIMSAAFI <7715@holbertonstudents.com>
2024-02-04 22:40:02 +0100

    Add tests/3-say_my_name.txt

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -22,10 +22,13 @@ def say_my_name(first_name, last_name=""):
     Returns:
     - None
     """
+
+    # Check if first_name is a string
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
 
+    # Check if last_name is a string
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-
+    # Print the name
     print("My name is {}$".format(first_name + " " + last_name))
