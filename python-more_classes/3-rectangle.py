2cc55a3ed013c9455da46de47748526b0fbc6ae8
NACIMSAAFI <7715@holbertonstudents.com>
2024-02-06 21:30:24 +0100

    Add 3-rectangle.py

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -22,6 +22,8 @@ class Rectangle:
         return 2 * (self.width + self.height)
 
     def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
         rectangle_str = ""
         for _ in range(self.height):
             rectangle_str += "#" * self.width + "\n"
