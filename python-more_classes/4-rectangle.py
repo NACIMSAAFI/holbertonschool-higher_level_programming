ee5c32406318fa95f6951313e46072cd5466a0e7
NACIMSAAFI <7715@holbertonstudents.com>
2024-02-06 21:43:05 +0100

    Add 4-rectangle.py

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.width = width
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0 and self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        rectangle_str = ""
+        for _ in range(self.height):
+            rectangle_str += "#" * self.width + "\n"
+        return rectangle_str.rstrip("\n")
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.width, self.height)
