1e5da80719084254bd009de373059b8c6a8c01b6
NACIMSAAFI <7715@holbertonstudents.com>
2024-02-07 18:40:34 +0100

    Description to 1-rectangle.py

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,5 +1,30 @@
 #!/usr/bin/python3
+"""
+Rectangle Class: Defines a rectangle object with specified width and height.
+
+Attributes:
+    width_value (int): The width of the rectangle.
+    height_value (int): The height of the rectangle.
+
+Methods:
+    __init__(self, width=0, height=0): Initializes a Rectangle object
+    with specified width and height.
+"""
+
+
 class Rectangle:
+    """
+    Initializes a Rectangle object with specified width and height.
+
+    Args:
+        width (int): The width of the rectangle (default is 0).
+        height (int): The height of the rectangle (default is 0).
+
+    Raises:
+        TypeError: If width or height is not an integer.
+        ValueError: If width or height is less than 0.
+    """
+
     def __init__(self, width=0, height=0):
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
