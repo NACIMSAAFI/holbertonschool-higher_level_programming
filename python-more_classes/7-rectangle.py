9ece9ebb06c74749ed7cddcd654776e2ba2f8543
NACIMSAAFI <7715@holbertonstudents.com>
2024-02-07 21:32:56 +0100

    Add 7-rectangle.py

---
+++ python-more_classes/7-rectangle.py
@@ -0,0 +1,73 @@
+class Rectangle:
+    """
+    Class representing a rectangle.
+    """
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """
+        Initializes a rectangle with the given width and height.
+
+        Args:
+            width (int, optional): The width of the rectangle. Defaults to 0.
+            height (int, optional): The height of the rectangle. Defaults to 0.
+        """
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """Getter method for width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method for width."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter method for height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method for height."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculates the area of the rectangle."""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Calculates the perimeter of the rectangle."""
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """Returns a string representation of the rectangle."""
+        if self.width == 0 or self.height == 0:
+            return ""
+        return "\n".join(
+            [str(self.print_symbol) * self.width for _ in range(self.height)]
+        )
+
+    def __repr__(self):
+        """Returns a string representation of the rectangle
+        for reproduction."""
+        return f"Rectangle({self.width}, {self.height})"
+
+    def __del__(self):
+        """Prints a message when an instance of Rectangle is deleted."""
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
