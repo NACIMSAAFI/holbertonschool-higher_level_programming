e48750d613dda0e33a6fc54d11cd64c680e5668f
NACIMSAAFI <7715@holbertonstudents.com>
2024-02-10 13:35:39 +0100

    Add 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,9 +5,10 @@ Class MyList that inherits from list.
 
 
 class MyList(list):
+    """
+    Prints the elements of the list in sorted order.
+    """
+
     def print_sorted(self):
-        """
-        Prints the elements of the list in sorted order.
-        """
         sorted_list = sorted(self)
         print(sorted_list)
