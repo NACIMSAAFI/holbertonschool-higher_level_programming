3dcfd37937775caa71c0a8ebcd5ccdd8e5624364
NACIMSAAFI <7715@holbertonstudents.com>
2024-02-10 13:32:57 +0100

    Add 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 """
-File: 1-my_list.py
-Description: Defines a custom list class MyList
-that inherits from the built-in list class.
-             Provides a method print_sorted() to print
-             the elements of the list in sorted order.
+Class MyList that inherits from list.
 """
 
 
