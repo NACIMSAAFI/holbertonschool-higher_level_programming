05647486dd143bb5d472bf1ed194849c30c3c3ac
NACIMSAAFI <7715@holbertonstudents.com>
2024-02-10 23:33:28 +0100

    Add 6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Calculate the area of the geometric shape."""
+
+    def area(self):
+        raise Exception("area() is not implemented")
