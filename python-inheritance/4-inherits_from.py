5dc7e5885b0f3070a3b347918f8de2d2bebe96b8
NACIMSAAFI <7715@holbertonstudents.com>
2024-02-10 21:11:21 +0100

    Add 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -9,4 +9,4 @@ def inherits_from(obj, a_class):
     from the specified class.
     """
 
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
