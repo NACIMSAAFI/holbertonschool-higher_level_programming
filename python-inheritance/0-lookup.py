1ed6df81c845f3c7f50db9e865c7fcba687f2da6
NACIMSAAFI <7715@holbertonstudents.com>
2024-02-10 11:41:03 +0100

    Add 0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
+"""
+Function that returns the list of available attributes and methods
+of an object
+   """
+
+
 def lookup(obj):
+    """
+    Provides a comprehensive list of attributes and methods
+    associated with the given object.
+
+    Parameters:
+        obj (object): The object to inspect.
+
+    Returns:
+        list: A list containing the names of attributes and methods
+        accessible on the object.
+    """
     return dir(obj)
