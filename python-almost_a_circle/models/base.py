de255d82f448d2edf48d847b1e80afb62c9d568a
NACIMSAAFI <7715@holbertonstudents.com>
2024-02-13 20:09:27 +0100

    Add models/base.py

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""class will be the “base” of all other classes in this project. """
+class Base:
+    """
+    Represents a base class with a private class attribute __nb_objects
+    and an instance attribute id.
+    """
+
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """
+        Initializes a Base instance.
+
+        Args:
+            id (int, optional): The ID of the instance. If None, automatically assigns a unique ID.
+        """
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
