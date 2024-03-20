d460b28f8a9c6b884b8aa84a08ab6e802bc380b9
NACIMSAAFI <7715@holbertonstudents.com>
2024-03-20 12:46:03 +0100

    Add 5-unique_id.sql

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- Creates the table unique_id on MySQL server.
-CREATE TABLE IF NOT EXISTS id_not_null (id INT PRIMARY KEY UNIQUE NOT NULL DEFAULT 1, name  VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1 UNIQUE, name VARCHAR(256))
