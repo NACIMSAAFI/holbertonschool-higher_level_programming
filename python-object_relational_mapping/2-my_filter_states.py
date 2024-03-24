0e20e6ca8d9d43967ae588cdb28188d778919ac1
NACIMSAAFI <7715@holbertonstudents.com>
2024-03-24 09:01:09 +0100

    Add 2-my_filter_states.py

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Script takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument."""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        db=argv[3],
+        charset="utf8"
+    )
+    cursor = db.cursor()
+    state_name_searched = argv[4]
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY\
+    %s ORDER BY id ASC", (state_name_searched,))
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
