fa551bf27c050c91cd76079d3a62e8ab31da12a6
NACIMSAAFI <7715@holbertonstudents.com>
2024-03-24 20:24:40 +0100

    Add model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -7,7 +7,12 @@ from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
 class State(Base):
-    """Class representing a state"""
+    """Class representing a state
+
+        Attributes:
+            __tablename__ (str): The table name of the class
+            id (int): The State id of the class
+            name (str): The State name of the class"""
 
     __tablename__ = 'states'
 
