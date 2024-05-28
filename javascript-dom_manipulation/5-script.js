a17f9a49a33098451d64dc4e709b1dba107ba784
NACIMSAAFI <7715@holbertonstudents.com>
2024-05-28 08:07:31 +0100

    Add 5-script.js

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,4 @@
+document.getElementById('update_header').addEventListener('click', function() {
+    var headerElement = document.querySelector('header');
+    headerElement.textContent = 'New Header!!!';
+});
